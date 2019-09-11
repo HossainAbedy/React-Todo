030df07b95772b612d6fa26c8a2d515f65c9691f
Abedy <abedy.ewu@gmail.com>
2019-09-11 17:38:07 +0600

    react

---
+++ build/precache-manifest.53bcb4fe9c38bcb1a8e108818557cfe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ea0cd096e0ca4ccc466",
+    "url": "/static/js/main.0ea0cd09.chunk.js"
+  },
+  {
+    "revision": "d026a73ef64ff5ccb041",
+    "url": "/static/js/1.d026a73e.chunk.js"
+  },
+  {
+    "revision": "0ea0cd096e0ca4ccc466",
+    "url": "/static/css/main.047428c8.chunk.css"
+  },
+  {
+    "revision": "6d1d6b787b739b1f351d4db070735f31",
+    "url": "/index.html"
+  }
+];
